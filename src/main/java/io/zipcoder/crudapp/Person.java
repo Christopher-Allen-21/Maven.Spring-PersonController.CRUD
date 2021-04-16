291716eeea6cbf89b4d027070aa833ada1cb3e67
Chris Allen <callen7908@yahoo.com>
2021-04-15 20:12:31 -0400

    Person and Person Controller setup done

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,16 @@
+package io.zipcoder.crudapp;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int id;
+
+    public Person(String firstName,String lastName, int id){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+
+
+}
---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,29 @@
+package io.zipcoder.crudapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonController {
+    List<Person> personList = new ArrayList<>();
+
+    public Person createPerson(Person p){
+        return null;
+    }
+
+    public Person getPerson(int id){
+        return null;
+    }
+
+    public List<Person> getPersonList(){
+        return null;
+    }
+
+    public Person updatePerson(Person p){
+        return null;
+    }
+
+    public void deletePerson(int id){
+
+    }
+
+}
